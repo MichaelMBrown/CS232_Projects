e4bc846499321c790b6aac4223ebe697cb9c3354
Michael Brown <michael@excentrix.net>
2021-10-13 01:05:37 -0400

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/proj1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj1.dir/crawler.c.o"
+  "proj1"
+  "proj1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/proj1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
